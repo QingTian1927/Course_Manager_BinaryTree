b367e594a69f52116256febb2c5fe0b817154c2f
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2024-10-14 09:30:15 +0700

    feat: thêm lớp Menu & bổ sung main.
    * Thêm chức năng cho lớp Menu và viết code khung logic cho main.

--- src/main/java/org/example/CourseManager.java
+++ src/main/java/org/example/CourseManager.java
@@ -1,7 +1,127 @@
 package org.example;
 
+import org.example.ui.Menu;
+import org.example.util.Validation;
+
 public class CourseManager {
     public static void main(String[] args) {
+        boolean isRunning = true;
+        while (isRunning) {
+            Menu.printMenu();
+            String choice = Validation.getString();
+            System.out.println();
 
+            switch (choice) {
+                case "1":
+                    manageCourseTree();
+                    break;
+                case "2":
+                    manageStudentTree();
+                    break;
+                case "3":
+                    manageRegisterList();
+                    break;
+                case "0":
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void manageCourseTree() {
+        while (true) {
+            Menu.printCourseMenu();
+            String choice = Validation.getString();
+            System.out.println();
+
+            switch (choice) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                case "9":
+                    break;
+                case "10":
+                    break;
+                case "11":
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void manageStudentTree() {
+        while (true) {
+            Menu.printStudentMenu();
+            String choice = Validation.getString();
+            System.out.println();
+
+            switch (choice) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void manageRegisterList() {
+        while (true) {
+            Menu.printRegisterMenu();
+            String choice = Validation.getString();
+            System.out.println();
+
+            switch (choice) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/org/example/ui/Menu.java
+++ src/main/java/org/example/ui/Menu.java
@@ -1,5 +1,57 @@
 package org.example.ui;
 
 public final class Menu {
-    private Menu() {}
+    private Menu() {
+    }
+
+    public static void printMenu() {
+        System.out.println("CSD201 BinaryTree CourseManager");
+        System.out.println("===============================\n");
+        System.out.println("1. Manage Course Tree");
+        System.out.println("2. Manage Student Tree");
+        System.out.println("3. Manage Register List");
+        System.out.println("0. Exit\n");
+        System.out.print("Enter your choice: ");
+    }
+
+    public static void printCourseMenu() {
+        System.out.println("----- Manage Course Tree -----\n");
+        System.out.println("1. Input & add to tree");
+        System.out.println("2. Display data by pre-order traversal");
+        System.out.println("3. Save course tree by post-order traversal");
+        System.out.println("4. Search by ccode");
+        System.out.println("5. Delete bcode by copying");
+        System.out.println("6. Delete bcode by merging");
+        System.out.println("7. Balance tree");
+        System.out.println("8. Display data by breadth-first traversal");
+        System.out.println("9. Count courses");
+        System.out.println("10. Search course by name");
+        System.out.println("11. Search course by code");
+        System.out.println("0. Exit\n");
+        System.out.print("Enter your choice: ");
+    }
+
+    public static void printStudentMenu() {
+        System.out.println("----- Manage Student Tree -----\n");
+        System.out.println("1. Input & add to tree");
+        System.out.println("2. Display data by in-order traversal");
+        System.out.println("3. Save student tree by post-order traversal");
+        System.out.println("4. Search by scode");
+        System.out.println("5. Delete scode by copying");
+        System.out.println("6. Search by name");
+        System.out.println("7. Search registered course by scode");
+        System.out.println("0. Exit\n");
+        System.out.print("Enter your choice: ");
+    }
+
+    public static void printRegisterMenu() {
+        System.out.println("----- Manage Register List -----\n");
+        System.out.println("1. Input & add to the end");
+        System.out.println("2. Display data");
+        System.out.println("3. Save register list to file");
+        System.out.println("4. Sort by ccode and scode");
+        System.out.println("5. Update mark by ccode and scode");
+        System.out.println("0. Exit\n");
+        System.out.print("Enter your choice: ");
+    }
 }
